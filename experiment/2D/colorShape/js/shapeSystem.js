be0d9b540ccae911be4c05cc27b37b35beab02d6
野狩 <ningyi.zny@alibaba-inc.com>
2014-09-01 14:52:39 +0800

    colorshape

--- experiment/2D/colorShape/js/shapeSystem.js
+++ experiment/2D/colorShape/js/shapeSystem.js
@@ -19,12 +19,16 @@
   var x = 0;
   var y = 0;
 
+  var circles = [];
+  var width;
+  var height;
+
+  var ep;
+
   ///////////////////////////////////////////////////////////////////////// 
   //////////////////////////////////shape//////////////////////////////////
   /////////////////////////////////////////////////////////////////////////
-  var Shape = function(paper, c, options) {
-    var ep = this.ep = options.ep;
-    this.c = c;
+  var Shape = function(paper, c) {
 
     var index = this.index = c.index;
     var fillColor = c.color;
@@ -42,46 +46,78 @@
       'stroke':null,
       'fill':fillColor,
       'fill-opacity':opacity,
-    }).hover(function(){
-      this.attr({'fill-opacity':0.8});
-      this.toFront();
-    },function(){
-      this.attr({'fill-opacity':opacity});
-    }).click(function(){
-      self.ep.emit('select',{circle:this,c:c});
     })
-    ;
-
-    circle.node.setAttribute('cursor','pointer')
-    circle.node.setAttribute('transition','all 0.3s ease-in-out');
+    .hover(function(){
+      self.reset(circles);
+      this.toFront();
+      this.node.setAttribute('class','circle-animation');
+    },function(){
+      var cx = this.attr('cx');
+      var cy = this.attr('cy');
+      self.shakes(circles,cx,cy);
+    }).click(function(){
+      this.select = true;
+      ep.emit('select',this);
+    });
+    circle.select = false;
+    circle.node.setAttribute('class','circle');
+    circles.push(circle);
+    circle.c = c;
   }
 
   /////////////////////shape之内的更新///////////////////
   Shape.prototype.update = function(t) {
-    this.r = parseInt((Math.cos(t*50+this.index)+1.1)*80);
+    this.r = 80//parseInt((Math.cos(t*50+this.index)+5)*20);
     this.x = this.width * Math.abs(Math.cos(Math.sin(t*this.speed)  + this.index * 100000));
     this.y = this.height * Math.abs(Math.sin(Math.cos(t*this.speed)   + this.index * this.index));
-  }
+  };
+
   Shape.prototype.render = function(t) {
     this.update(t);
-
     this.circle.attr({
-      cx:this.x,
-      cy:this.y,
-      r:this.r,
+      'cx':this.x,
+      'cy':this.y,
+      'r' :this.r,
       'z-index':this.r,
     })
   }
+
+  Shape.prototype.reset = function(circles) {
+    for (var k in circles){
+      var circle = circles[k];
+      circle.node.setAttribute('class','circle')}
+    }  
+
+  Shape.prototype.shakes = function(circles, cx, cy) {
+    for (var k in circles){
+      var circle = circles[k];
+      this.shake(circle, cx, cy);
+    }
+  }
+
+  Shape.prototype.shake = function(circle, cx, cy) {
+    var cx1 = circle.attr('cx');
+    var cy1 = circle.attr('cy');
+    var dist = Math.abs(cx-cx1)+Math.abs(cy-cy1);
+    var d = dist/(width+height);
+    var delay = (d*5)*300;
+    setTimeout(function(){circle.node.setAttribute('class','circle-animation-all')},delay);
+    // setTimeout(function(){circle.node.setAttribute('class','circle')},5000);
+  };
 
   ////////////////////////////////////////////////////////
   ////////////////////////ShapeSystem////////////////////////
   ////////////////////////////////////////////////////////
   var self;
-  var ShapeSystem = function(node,cArr) {
+  var ShapeSystem = function(node,cArr,options) {
     var self = this;
     var node = $(node);
+    iMax = options.iMax||10;
+    jMax = options.jMax||5;
     this.node = node;
     this.cArr = cArr;
+    width = node.width();
+    height = node.height();
     var shapeN = this.shapeN = cArr.length;
     
     // this.initCanvas();
@@ -98,6 +134,7 @@
     this.gridW = Math.sqrt(this.width * this.height / 100);
   }
 
+
   ShapeSystem.prototype.event = function() {
     var self = this;
     this.node
@@ -105,10 +142,14 @@
       .mouseover(onMouseOver)
       .mouseout(onMouseOut);
 
-    var ep = this.ep = new EventProxy();
-    ep.on('select',function(data){
+    ep = new EventProxy();
+    ep.on('select',function(center){
       self.stop();
-      self.rank();
+      for(var k in circles){
+        var circle = circles[k];
+        explode(circle, center);
+      }
+      rank(center);
     })
   }
 
@@ -129,8 +170,7 @@
     var node = this.node;
     var width = this.width = node.width();
     var height = this.height = node.height();
-    this.paper = new Raphael(node[0],width,height);
-
+    paper = this.paper = new Raphael(node[0],width,height);
   }
   ShapeSystem.prototype.resize = function() {
     this.node.empty();
@@ -138,6 +178,16 @@
     this.parameters();
     this.initShapes();
   }
+
+   ShapeSystem.prototype.shrink = function() {
+    // var anim = Raphael.animation({
+    //   'cx': pos.cx,
+    //   'cy': pos.cy,
+    //   'r': r,
+    //   'fill-opacity': 1,
+    // }, time, 'bounce', rankOther).delay(delay);
+    // this.animate(anim);
+   }
 
   ShapeSystem.prototype.initShapes = function(shapeN) {
     var cArr = this.cArr;
@@ -196,32 +246,99 @@
     this.loopId = window.requestAnimationFrame(function(){self.loop()});
   }
 
-
-  ShapeSystem.prototype.rank = function() {
-    var shapes = this.shapes;
-    for(var k in shapes){
-      var obj = shapes[k];
-      var circle = obj.circle;
-
-      var c = obj.c;
+  function getPosition(circle){
+      var dx = width/iMax;
+      var dy = 30;
+      var c = circle.c;
       var i = c.i;
       var j = c.j;
-      var index = c.index;
-      var time = (1+Math.random())*300;
-      var delay = index*(1+Math.random())*2;
+      var obj = {};
+      obj.cx = i*dx +dx/2;
+      obj.cy = j*dy+30;
 
-      var anim = Raphael.animation({
-        'cx':i*60,
-        'cy':j*60+30,
-        'r':8,
-        'fill-opacity':1,
-      }, time).delay(delay);
-
-      circle.animate(anim); 
-      circle.animateWith({ease:'easeInOut'});
-    }
+      return obj;
   }
 
+ 
+  curJ = null;
+  function rank(circle) {
+    var r = 12;
+    // var index = c.index;
+    var time = 1000;
+    var delay = 300;
+    curJ = circle.c.j;
+
+    var pos = getPosition(circle);
+
+    var anim = Raphael.animation({
+      'cx': pos.cx,
+      'cy': pos.cy,
+      'r': r,
+      'fill-opacity': 1,
+    }, time, 'bounce', rankOther).delay(delay);
+
+    circle.animate(anim);
+
+    function rankOther() {
+      var time = 300;
+      var r = 6;
+      for (var k in circles) {
+        var circle = circles[k];
+        if (circle.c.j == curJ && !circle.select) {
+          var i = circle.c.i;
+          var delay = i *20;
+          var pos = getPosition(circle);
+          var anim = Raphael.animation({
+            'cx': pos.cx,
+            'cy': pos.cy,
+            'r': r,
+            'fill-opacity': 1,
+          }, time, 'easeInOut',function(){
+            paper.setSize(paper.width,300)
+          })
+          .delay(delay);
+          circle.animate(anim);
+        }
+      }
+    }
+  };
+
+  function explode(circle, center){
+    var centerX = center.attr('cx');
+    var centerY = center.attr('cy');
+    var r = 1900;
+    // var phi = Math.random()*6.28;
+    if(!circle.select){
+      var x = circle.attr('cx');
+      var y = circle.attr('cy');
+      var dx = (x - centerX);
+      var dy = (y - centerY);
+      var dr = Math.sqrt(dx*dx+dy*dy);
+
+      function out() {
+        var time = 1000;
+        var anim = Raphael.animation({
+          'cx': centerX + r * dx/dr,
+          'cy': centerY + r * dy/dr,
+        }, time, 'easeInOut')
+        circle.animate(anim);
+      }
+
+      function small() {
+        var time = 400;
+        var delay = (1 + Math.random()) * 20;
+        var anim = Raphael.animation({
+          'cx': x + r * dx/dr/50,
+          'cy': y + r * dy/dr/50,
+          'r': 8,
+          'fill-opacity': 1,
+        }, time, 'bounce',out).delay(delay);
+        circle.animate(anim);
+      }
+
+      small();
+    }
+  }
   var xMax = 2000;
   var yMax = 500;
   function onMouseMove(e) {
